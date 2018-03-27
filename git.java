208edb7fcd123bfade6686782fa0cf6c90c726c7
sushilathapa <susilamagaro52@gmail.com>
2018-03-27 15:03:33 +0545

    beginning is the best way to start a new.

--- git.java
+++ git.java
@@ -1,2 +1,13 @@
-hello i am a learner.
-life is good.
+
+public class Box {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int length=2;
+		int breadth=3;
+		int height=5;
+		int vol=length*breadth*height;
+		System.out.println("volume of box is:"+vol);
+	}
+
+}
